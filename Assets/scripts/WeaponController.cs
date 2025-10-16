999ee92d8344ef19ab18c4177269298cdd47cc76
Nathan147-dotcom <nathanmorales12mntp@gmail.com>
2025-10-16 15:50:51 -0700

    shooting

---
+++ Assets/scripts/WeaponController.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+
+{
+
+    public Transform firePoint;
+    public GameObject ammoType;
+    
+    public float shotSpeed;
+    public float shotCounter;
+    public float fireRate;
+    
+    private Animator playerAnim;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0)){
+            shotCounter -= Time.deltaTime;
+            if (shotCounter<=0){
+                shotCounter = fireRate;
+                Shoot();
+            }
+        }
+        else{
+            shotCounter = 0;
+        }
+    }
+    //Prefab
+    void Shoot(){
+
+        
+        GameObject shot = Instantiate(ammoType, firePoint.position, firePoint.rotation);
+        Rigidbody2D shotRb = shot.GetComponent<Rigidbody2D>();
+        shotRb.AddForce(firePoint.right * shotSpeed * playerDirection(), ForceMode2D.Impulse);
+        Destroy(shot.gameObject, 1f);
+    }
+    int playerDirection(){
+
+            if(transform.parent.localScale.x<0f){
+                return -1;
+            }
+            else{
+                return +1;
+            }
+        }
+}
