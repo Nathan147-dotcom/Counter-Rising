190a8a57dab26af534ec27ce54d0972390d27d85
Nathan147-dotcom <nathanmorales12mntp@gmail.com>
2025-10-21 18:09:08 -0700

    player now constantly takes damage

--- Assets/scripts/EnemyDamage.cs
+++ Assets/scripts/EnemyDamage.cs
@@ -4,10 +4,15 @@ public class EnemyDamage : MonoBehaviour
 {
 
     public int enemyDamage;
+    public float damageDelay = 1f;
+    private float lastDamageTime;
     
-    void OnTriggerEnter2D(Collider2D other){
+    void OnTriggerStay2D(Collider2D other){
         if(other.gameObject.tag == "Player"){
+            if(Time.time > lastDamageTime + damageDelay){
             other.gameObject.GetComponent<PlayerHealth>().damagePlayer(enemyDamage);
+            lastDamageTime = Time.time;
+            }
         }
     }
 }
