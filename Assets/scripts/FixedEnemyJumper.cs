b0b05e7b950b1c4a7361b485bf89a6a4e709d1bc
Nathan147-dotcom <nathanmorales12mntp@gmail.com>
2025-10-22 01:05:02 -0700

    new background

--- Assets/scripts/FixedEnemyJumper.cs
+++ Assets/scripts/FixedEnemyJumper.cs
@@ -8,10 +8,18 @@ public class FixedEnemyJumper : MonoBehaviour
     public float jumpCooldown;
     public float jumpImpulse;
     float lastJumpTime;
+    private bool isGrounded;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;   // empty child under the feet
+    [SerializeField] private Vector2 groundBoxSize = new Vector2(0.8f, 0.1f);
+    [SerializeField] private LayerMask groundLayer = ~0; // set to your Ground layer
+
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapBox(groundCheck.position, groundBoxSize, 0f, groundLayer);
         float dist = Vector2.Distance(player.transform.position, transform.position);
         Vector3 scale = transform.localScale;
 
@@ -24,13 +32,26 @@ public class FixedEnemyJumper : MonoBehaviour
         }
 
         transform.localScale = scale;
+        
+        Debug.Log("isGrounded = " + isGrounded);
 
-        if (Time.time > lastJumpTime + jumpCooldown) {
+        if ( isGrounded && Time.time > lastJumpTime + jumpCooldown) {
 
                 lastJumpTime = Time.time;
 
                 GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpImpulse, ForceMode2D.Impulse);
         }
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireCube(groundCheck.position, groundBoxSize);
+        }
+        Gizmos.color = new Color(0f, 0.5f, 1f, 0.15f);
+        Gizmos.DrawWireSphere(transform.position, detectRadiusRange);
     }
 }
