a2704f69cd3382b3cdc8b003bfcde54564cf0e46
Nathan147-dotcom <nathanmorales12mntp@gmail.com>
2025-10-25 00:23:30 -0700

    skeleton animations

--- Assets/scripts/EnemyMovement.cs
+++ Assets/scripts/EnemyMovement.cs
@@ -6,11 +6,14 @@ public class enemies : MonoBehaviour
     public bool flip;
     public float speed;
     public float detectRadiusRange;
+    private Animator animator;
+    private bool moving;
 
     void Awake(){
         if(player == null){
             player = GameObject.FindWithTag("Player");
         }
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,16 +22,21 @@ public class enemies : MonoBehaviour
         Vector3 scale = transform.localScale;
         float dist = Vector2.Distance(player.transform.position, transform.position);
         if(dist < detectRadiusRange){
-        if(player.transform.position.x > transform.position.x){
-            scale.x = Mathf.Abs(scale.x) * -1 * (flip ? -1 : 1);
-            transform.Translate(speed * Time.deltaTime, 0, 0);
+            moving = true; 
+            animator.SetBool("moving", moving);
+            if(player.transform.position.x > transform.position.x){
+                scale.x = Mathf.Abs(scale.x) * -1 * (flip ? -1 : 1);
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+                }
+                else{
+                    scale.x = Mathf.Abs(scale.x) * (flip ? -1: 1);
+                    transform.Translate(speed * Time.deltaTime * -1, 0, 0);
+                    }
+                    transform.localScale = scale;
         }
         else{
-            scale.x = Mathf.Abs(scale.x) * (flip ? -1: 1);
-            transform.Translate(speed * Time.deltaTime * -1, 0, 0);
-        }
-
-        transform.localScale = scale;
+            moving = false;
+            animator.SetBool("moving", moving);
         }
     }
 }
